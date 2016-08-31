c29fbbe1fda2612a99a578c6c751132d36a91670
David Barreto <barretollano@gmail.com>
2016-08-31 12:56:35 -0400

    Initial commit

---
+++ src/app/root/root.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'root',
+  styles: [require('./root.component.css')],
+  template: require('./root.component.html')
+})
+export class RootComponent {}
