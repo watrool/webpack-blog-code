7000dca310eb136293bed78aff97750e7a1b4d06
troy.tao <troy.tao@rangle.io>
2016-10-13 15:48:53 -0400

    Apply AoT

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppModuleNgFactory } from './app/app.module.ngfactory';
 
 enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
