910632cc3780ccf9494ee46b1fd21b121928a7ec
troy.tao <troy.tao@rangle.io>
2016-10-25 12:04:48 -0400

    Minor Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
-import { styles } from "./index.aot.css"
 @Component({
   selector: 'rio-app',
-  styles: styles,
-  template: '<p class="bg-blue white">Hello World!</p>'
+  template: '<p>Hello World!</p>'
 })
 export class AppComponent {}
